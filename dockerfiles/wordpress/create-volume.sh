299fbdbddd1ac4c6b768a66d377a8242b5bdb4ba
Juergen Seliger <juergen.seliger@gmail.com>
2017-12-28 01:12:59 +0100

    volume angepasst

--- dockerfiles/wordpress/create-volume.sh
+++ dockerfiles/wordpress/create-volume.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker volume create --driver local --opt device=:/home/juergen/dev/docker/volumes/wp-database wp-database
-docker volume create --driver local --opt device=:/home/juergen/dev/docker/volumes/wp-files wp-files
+docker volume create --driver local --opt device=:/home/juergen/dev/docker/volumes/wp-database wordpress_wp-database
+docker volume create --driver local --opt device=:/home/juergen/dev/docker/volumes/wp-files wordpress_wp-files
