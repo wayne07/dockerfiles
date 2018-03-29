49da2d0b1deafe14ca0f5a23d3cca793834259a8
Juergen Seliger <juergen.seliger@crafting-it.de>
2018-03-29 16:38:55 +0200

    use new image jenkins-2.113

--- dockerfiles/jenkins/jenkins-start.sh
+++ dockerfiles/jenkins/jenkins-start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -p 8080:8080 -p 50000:50000 --name jenkins -v /home/juergen/dev/docker/jenkins-config:/var/jenkins_home wayne07/dockerfiles
+docker run -d -p 8080:8080 -p 50000:50000 --name jenkins -v /home/juergen/dev/docker/jenkins-config:/var/jenkins_home wayne07/dockerfiles:jenkins-2.113
