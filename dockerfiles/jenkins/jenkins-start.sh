4b626c7bdb668f07f8e2d9467a4b4bc2da43080a
Juergen Seliger <juergen.seliger@gmail.com>
2018-04-01 14:11:44 +0200

    volume für jdk eingebunden

--- dockerfiles/jenkins/jenkins-start.sh
+++ dockerfiles/jenkins/jenkins-start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -p 8080:8080 -p 50000:50000 --name jenkins -v /home/juergen/dev/docker/jenkins-config:/var/jenkins_home wayne07/dockerfiles:jenkins-2.113
+docker run -d -p 8080:8080 -p 50000:50000 --name jenkins -v /home/juergen/dev/docker/jenkins-config:/var/jenkins_home -v /home/juergen/dev/docker/java:/opt/java wayne07/dockerfiles:jenkins-2.113-with-gradle
