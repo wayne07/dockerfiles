574f826387b07ec1d37ab75912c6a637e4b12451
Juergen Seliger <juergen.seliger@gmail.com>
2018-05-08 22:20:40 +0200

    Port angepasst

--- dockerfiles/wordpress/anpassung.sql
+++ dockerfiles/wordpress/anpassung.sql
@@ -1,3 +1,3 @@
 SELECT * FROM `wp_options` WHERE `option_name` = 'siteurl' or `option_name` = 'home'
 
-UPDATE `wp_options` SET `option_value`='http://localhost:8000' WHERE `option_name` = 'siteurl' or `option_name` = 'home'
+UPDATE `wp_options` SET `option_value`='http://localhost:9000' WHERE `option_name` = 'siteurl' or `option_name` = 'home'
