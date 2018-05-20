af9d41ea46fbf0b374d91b51b216cfa4725e0c77
Juergen Seliger <juergen.seliger@gmail.com>
2018-05-20 12:31:26 +0200

    Child Theme Twentyseventeen hinzugefügt

---
+++ wordpress/themes/twentyseventeen-child/templateBfW.php
@@ -0,0 +1,82 @@
+<?php
+/**
+* Template Name: templateBfW.php
+*
+*
+* @package Twenty_Seventeen
+*/
+
+get_header(); ?>
+
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/page/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+/**
+*   Anpassung
+**/
+        <?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+        $args = array(
+            'category_name' => 'Ortschaftsrat',
+            'paged' => $paged,
+            'posts_per_page' => 5
+        );
+
+        query_posts($args); ?>
+
+		<?php
+		if ( have_posts() ) :
+		?>
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/post/content', get_post_format() );
+
+			endwhile;
+
+			the_posts_pagination(
+				array(
+					'prev_text'          => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
+					'next_text'          => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
+					'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
+				)
+			);
+
+		else :
+
+			get_template_part( 'template-parts/post/content', 'none' );
+
+		endif;
+		?>
+/**
+*   ENDE: Anpassung
+**/
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+
+<?php
+get_footer();
