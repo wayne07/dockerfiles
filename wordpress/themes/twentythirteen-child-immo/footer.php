c617f3e8368f25263c730f4f09f481b4d7aa101f
Juergen Seliger <juergen.seliger@crafting-it.de>
2018-06-01 15:37:08 +0200

    screenshot hinzugefügt, footer korrigiert

--- wordpress/themes/twentythirteen-child-immo/footer.php
+++ wordpress/themes/twentythirteen-child-immo/footer.php
@@ -15,15 +15,20 @@
 			<?php get_sidebar( 'main' ); ?>
 
 			<div class="site-info">
-				<?php do_action( 'twentythirteen_credits' ); ?>
-				<?php
-				if ( function_exists( 'the_privacy_policy_link' ) ) {
-					the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
-				}
-				?>
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentythirteen' ) ); ?>" class="imprint">
-					<?php printf( __( 'Proudly powered by %s', 'twentythirteen' ), 'WordPress' ); ?>
-				</a>
+                <a href="<?php echo site_url();?>"> Startseite</a>
+                <span role="separator" aria-hidden="true"></span>
+
+                <?php
+                if ( function_exists( 'the_privacy_policy_link' ) ) {
+                    the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
+                }
+                ?>
+
+                <a href="<?php echo site_url( '/impressum/');?>"> Impressum</a>
+                <span role="separator" aria-hidden="true"></span>
+
+                <a href="<?php echo site_url( '/kontakt/');?>" > Kontakt</a>
+
 			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
 	</div><!-- #page -->
